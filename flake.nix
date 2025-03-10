af7c5c1ef31741a3715916760bb53e8f2091e98f
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-11 00:40:59 +0400

    Move nixos and home-manager configurations

--- flake.nix
+++ flake.nix
@@ -12,13 +12,13 @@
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	modules = [ 
-	  ./configuration.nix
+	  ./nixos/configuration.nix
 
 	  home-manager.nixosModules.home-manager {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
 
-	    home-manager.users.nikko = import ./home.nix;
+	    home-manager.users.nikko = import ./home-manager/home.nix;
 	  }
 	];
       };
