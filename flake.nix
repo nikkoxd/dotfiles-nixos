6c1c2f2f0945c4a68feea3a9c6da4955f03ca64a
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-10 23:56:00 +0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "NixOS System Config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
