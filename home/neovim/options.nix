eddf13fb52e6d6433f9421cfae1d183f188848f2
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-12 15:55:34 +0400

    Set neovim options

--- home/neovim/options.nix
+++ home/neovim/options.nix
@@ -3,8 +3,38 @@
     enableMan = true;
     colorscheme = "rose-pine";
     opts = {
+      # Line numbers
       number = true;
       relativenumber = true;
+
+      # Mouse control
+      mouse = "a";
+      mousemodel = "extend";
+
+      # Splits
+      splitbelow = true;
+      splitright = true;
+
+      # Search
+      incsearch = true;
+      inccommand = "split";
+      ignorecase = true;
+      smartcase = true;
+
+      # Tab options
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      autoindent = true;
+
+      scrolloff = 8;
+      cursorline = true;
+      cursorcolumn = false;
+      signcolumn = "yes";
+
+      wrap = true;
+
+      termguicolors = true;
     };
   };
 }
