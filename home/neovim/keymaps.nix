befdb9c9c7c626b8452adf317d1d66d118c926be
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-12 16:58:09 +0400

    Add leader key mapping

--- home/neovim/keymaps.nix
+++ home/neovim/keymaps.nix
@@ -1,11 +1,17 @@
 { ... }: {
-  programs.nixvim.keymaps = [
-    {
-      action = "<cmd>Neogit<CR>";
-      key = "<leader>gg";
-      options = {
-        silent = true;
-      };
-    }
-  ];
+  programs.nixvim = {
+    globals = {
+      mapleaeder = " ";
+      maplocalleader = " ";
+    };
+    keymaps = [
+      {
+        action = "<cmd>Neogit<CR>";
+        key = "<leader>gg";
+        options = {
+          silent = true;
+        };
+      }
+    ];
+  };
 }
