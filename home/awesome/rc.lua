2a9de166dac8827fc12b9afe3d6968e2a2684462
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-13 16:37:27 +0400

    Make tile layout the default

--- home/awesome/rc.lua
+++ home/awesome/rc.lua
@@ -68,7 +68,6 @@ local modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	awful.layout.suit.floating,
 	awful.layout.suit.tile,
 	awful.layout.suit.tile.left,
 	awful.layout.suit.tile.bottom,
@@ -81,6 +80,7 @@ awful.layout.layouts = {
 	awful.layout.suit.max.fullscreen,
 	awful.layout.suit.magnifier,
 	awful.layout.suit.corner.nw,
+	awful.layout.suit.floating,
 	-- awful.layout.suit.corner.ne,
 	-- awful.layout.suit.corner.sw,
 	-- awful.layout.suit.corner.se,
