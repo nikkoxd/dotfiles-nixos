7b58c7397eb0853f9658e884c80847add1b38ca0
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-13 20:38:41 +0400

    Change ghostty font and add padding

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -4,6 +4,11 @@
 
     settings = {
       theme = "tokyonight";
+      font-family = "Iosevka Nerd Font";
+
+      window-padding-x = 10;
+      window-padding-y = 10;
+
       window-decoration = false;
       gtk-single-instance = true;
     };
