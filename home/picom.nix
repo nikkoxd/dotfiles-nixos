c863c7b763c57dc2941419143e8692b45c076cac
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-14 22:04:43 +0400

    Add blur, use glx for backend

--- home/picom.nix
+++ home/picom.nix
@@ -1,7 +1,15 @@
 { ... }: {
   services.picom = {
     enable = true;
+    backend = "glx";
     activeOpacity = 0.95;
     inactiveOpacity = 0.9;
+    settings = {
+      blur = {
+        method = "gaussian";
+        size = 10;
+        deviation = 5.0;
+      };
+    };
   };
 }
