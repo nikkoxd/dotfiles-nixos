979a1a111f644e3df20bceba6ee8137f2ae8f2f7
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-15 00:07:45 +0400

    Add shadows, corner radius and animations

--- home/picom.nix
+++ home/picom.nix
@@ -1,10 +1,29 @@
-{ ... }: {
+{ lib, ... }: {
   services.picom = {
     enable = true;
-    backend = "glx";
-    activeOpacity = 0.95;
-    inactiveOpacity = 0.9;
+
     settings = {
+      backend = "glx";
+
+      shadow = true;
+      shadow-radius = 12;
+      shadow-opacity = 1;
+      shadow-offset-x = -12;
+      shadow-offset-y = -12;
+      shadow-color = "#000000";
+
+      active-opacity = 0.9;
+      inactive-opacity = 0.9;
+
+      # fading = true;
+      # fade-in-step = 0.5;
+      # fade-out-step = 0.5;
+
+      corner-radius = 10;
+      rounded-corners-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+      ];
       blur = {
         method = "gaussian";
         size = 10;
@@ -12,4 +31,11 @@
       };
     };
   };
+
+  xdg.configFile."picom/picom.conf".text = lib.mkAfter ''
+    animations = ({
+      triggers = [ "open", "close", "show", "hide", "size", "position" ];
+      preset = "geometry-change";
+    })
+  '';
 }
