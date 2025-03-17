d5c1b10a74961bbf8fd61327c74a9a142b2eaff9
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-17 22:37:57 +0400

    Edit zellij keybinds

--- home/zellij.nix
+++ home/zellij.nix
@@ -6,9 +6,21 @@
       pane_frames = false;
       keybinds = {
         normal = {
-          "bind \"Ctrl m\"" = { SwitchToMode = "move"; };
+          "bind \"Alt r\"" = { SwitchToMode = "resize"; };
+          "bind \"Alt p\"" = { SwitchToMode = "pane"; };
+          "bind \"Alt m\"" = { SwitchToMode = "move"; };
+          "bind \"Alt t\"" = { SwitchToMode = "tab"; };
+          "bind \"Alt s\"" = { SwitchToMode = "search"; };
+          "bind \"Alt o\"" = { SwitchToMode = "session"; };
         };
-        unbind = "Ctrl h";
+        unbind = [
+          "Ctrl n"
+          "Ctrl p"
+          "Ctrl h"
+          "Ctrl t"
+          "Ctrl s"
+          "Ctrl o"
+        ];
       };
     };
   };
