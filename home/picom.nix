d73009bedc41a90bda9d154ccde91bb71e8892be
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-19 17:08:30 +0400

    Use rules for configuring picom

--- home/picom.nix
+++ home/picom.nix
@@ -5,25 +5,12 @@
     settings = {
       backend = "glx";
 
-      shadow = true;
       shadow-radius = 12;
       shadow-opacity = 1;
       shadow-offset-x = -12;
       shadow-offset-y = -12;
       shadow-color = "#000000";
 
-      active-opacity = 0.9;
-      inactive-opacity = 0.9;
-
-      # fading = true;
-      # fade-in-step = 0.5;
-      # fade-out-step = 0.5;
-
-      corner-radius = 10;
-      rounded-corners-exclude = [
-        "window_type = 'dock'"
-        "window_type = 'desktop'"
-      ];
       blur = {
         method = "gaussian";
         size = 10;
@@ -33,9 +20,28 @@
   };
 
   xdg.configFile."picom/picom.conf".text = lib.mkAfter ''
-    animations = ({
-      triggers = [ "open", "close", "show", "hide", "size", "position" ];
-      preset = "geometry-change";
-    })
+    rules = (
+      {
+        match = "window_type = 'desktop' || window_type = 'dock'";
+        blur-background = false;
+      },
+      { 
+        match = "window_type != 'desktop' && window_type != 'dock'"
+        opacity = 0.9; 
+        shadow = true; 
+        blur-background = true;
+      },
+    )
+    
+    animations = (
+      {
+        triggers = [ "open" ];
+        preset = "appear";
+      },
+      {
+        triggers = [ "close" ];
+        preset = "disappear";
+      }
+    )
   '';
 }
