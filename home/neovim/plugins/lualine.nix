a4ef6aad39953adb6280276ee0513e8e587d749e
nikkoxd <vladik.nekrasov3@gmail.com>
2025-03-19 17:09:21 +0400

    Edit lualine config

--- home/neovim/plugins/lualine.nix
+++ home/neovim/plugins/lualine.nix
@@ -1,11 +1,49 @@
 { ... }: {
   programs.nixvim.plugins.lualine = {
     enable = true;
+    luaConfig.pre = ''
+      local function bleh()
+        return ""
+      end
+    '';
     settings = {
       options = {
         component_separators = "";
         section_separators = "";
         globalstatus = true;
+      };
+      sections = {
+        lualine_a = [
+          {
+            __unkeyed-1 = "mode";
+            fmt.__raw = "string.lower";
+          }
+        ];
+        lualine_b = [
+          ""
+        ];
+        lualine_c = [
+          "diagnostics"
+          {
+            __unkeyed-1 = "filetype";
+            icon_only = true;
+            padding = {
+              left = 1;
+              right = 0;
+            };
+          }
+          "filename"
+        ];
+        lualine_x = [
+          "progress"
+          "location"
+        ];
+        lualine_y = [
+          ""
+        ];
+        lualine_z = [
+          ""
+        ];
       };
     };
   };
